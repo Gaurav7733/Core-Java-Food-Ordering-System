de375ef162dfaef8eb17695a22cd0f9060571bf1
Gaurav Ramchandra Yadav <118457315+Gaurav7733@users.noreply.github.com>
2023-12-07 12:18:11 +0530

    Update FoodOrderSystem.java

--- FoodOrderSystem.java
+++ FoodOrderSystem.java
@@ -1,76 +1,163 @@
-package Gaurav;
+package core_java_Food_Ordering_System;
 
 //FoodOrderSystem.java
 //FoodOrderSystem.java
 import java.util.Scanner;
 
 public class FoodOrderSystem {
- public static void main(String[] args) {
-     Scanner scanner = new Scanner(System.in);
-     Starters starters = new Starters();
-     MainCourse mainCourse = new MainCourse();
-     Desserts desserts = new Desserts();
-     Drinks drinks = new Drinks();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Starters starters = new Starters();
+        MainCourse mainCourse = new MainCourse();
+        Desserts desserts = new Desserts();
+        Drinks drinks = new Drinks();
 
-     System.out.println("Welcome to the Food Ordering System!");
-     System.out.println("Menu Options:");
-     System.out.println("1: Starters");
-     System.out.println("2: Main Course Recipes");
-     System.out.println("3: Desserts");
-     System.out.println("4: Drinks");
-     System.out.println("0: Exit");
-     Order[] orders = new Order[50];
-     int orderIndex = 0;
-     double totalBill = 0.0;
+        System.out.println("Welcome to the Food Ordering System!");
+        System.out.println("Menu Options:");
+        System.out.println("1: Starters");
+        System.out.println("2: Main Course Recipes");
+        System.out.println("3: Desserts");
+        System.out.println("4: Drinks");
+        System.out.println("0: Exit");
+        Order[] orders = new Order[50];
+        int orderIndex = 0;
+        double totalBill = 0.0;
 
-     while (true) {
-         System.out.print("Enter your choice (0 to exit): ");
-         int choice = scanner.nextInt();
+        while (true) 
+        {
+            System.out.print("Enter your choice (0 to exit): ");
+            int choice = scanner.nextInt();
 
-         if (choice == 0) {
-             break;
-         } else if (choice == 1) {
-             System.out.println("Starters:");
-             FoodItem[] starterItems = starters.getStarters();
-             int itemIndex = 1;
-             for (FoodItem item : starterItems) {
-                 System.out.println(itemIndex + ": " + item.getName() + " - Rs. " + item.getPrice());
-                 itemIndex++;
-             }
+            if (choice == 0) 
+            {
+                break;
+            } else if (choice == 1) 
+            {
+                System.out.println("Starters:");
+                FoodItem[] starterItems = starters.getStarters();
+                int itemIndex = 1;
+                for (FoodItem item : starterItems) 
+                {
+                    System.out.println(itemIndex + ": " + item.getName() + " - Rs. " + item.getPrice());
+                    itemIndex++;
+                }
 
-             System.out.print("Enter the item number: ");
-             int itemNumber = scanner.nextInt();
-             System.out.print("Enter the quantity: ");
-             int quantity = scanner.nextInt();
+                System.out.print("Enter the item number: ");
+                int itemNumber = scanner.nextInt();
+                System.out.print("Enter the quantity: ");
+                int quantity = scanner.nextInt();
 
-             if (itemNumber >= 1 && itemNumber <= starterItems.length) {
-                 orders[orderIndex] = new Order(starterItems[itemNumber - 1], quantity);
-                 totalBill += orders[orderIndex].getTotalPrice();
-                 orderIndex++;
-             } else {
-                 System.out.println("Invalid item number. Please try again.");
-             }
-         } else if (choice == 2) {
-             // Similar process for Main Course Recipes
-         } else if (choice == 3) {
-             // Similar process for Desserts
-         } else if (choice == 4) {
-             // Similar process for Drinks
-         } else {
-             System.out.println("Invalid choice. Please try again.");
-         }
-     }
+                if (itemNumber >= 1 && itemNumber <= starterItems.length) 
+                {
+                    orders[orderIndex] = new Order(starterItems[itemNumber - 1], quantity);
+                    totalBill += orders[orderIndex].getTotalPrice();
+                    orderIndex++;
+                } 
+                else 
+                {
+                    System.out.println("Invalid item number. Please try again.");
+                }
+            } 
+            else if (choice == 2) 
+            {
+                // Similar process for Main Course Recipes
+                System.out.println("Main Course Recipes:");
+                        FoodItem[] mainCourseItems = mainCourse.getMainCourses();
+                        int itemIndex = 1;
+                        for (FoodItem item : mainCourseItems) 
+                        {
+                            System.out.println(itemIndex + ": " + item.getName() + " - Rs. " + item.getPrice());
+                            itemIndex++;
+                        }
 
-     // Display the bill
-     System.out.println("Your order summary:");
-     for (int i = 0; i < orderIndex; i++) {
-         Order order = orders[i];
-         System.out.println(order.getItem().getName() + " x " + order.getQuantity() + " - Rs. " + order.getTotalPrice());
-     }
-     System.out.println("----------------------------");
-     System.out.println("Total Bill: Rs. " + totalBill);
-     System.out.println("Thank you for using the Food Ordering System!");
- }
+                        System.out.print("Enter the item number: ");
+                        int itemNumber = scanner.nextInt();
+                        System.out.print("Enter the quantity: ");
+                        int quantity = scanner.nextInt();
+
+                        if (itemNumber >= 1 && itemNumber <= mainCourseItems.length) 
+                        {
+                            orders[orderIndex] = new Order(mainCourseItems[itemNumber - 1], quantity);
+                            totalBill += orders[orderIndex].getTotalPrice();
+                            orderIndex++;
+                        } 
+                        else 
+                        {
+                            System.out.println("Invalid item number. Please try again.");
+                        }
+            } 
+            else if (choice == 3) 
+            {
+                // Similar process for Desserts
+            	System.out.println("Desserts:");
+                FoodItem[] dessertItems = desserts.getDesserts();
+                int itemIndex = 1;
+                for (FoodItem item : dessertItems) 
+                {
+                    System.out.println(itemIndex + ": " + item.getName() + " - Rs. " + item.getPrice());
+                    itemIndex++;
+                }
+
+                System.out.print("Enter the item number: ");
+                int itemNumber = scanner.nextInt();
+                System.out.print("Enter the quantity: ");
+                int quantity = scanner.nextInt();
+
+                if (itemNumber >= 1 && itemNumber <= dessertItems.length) 
+                {
+                    orders[orderIndex] = new Order(dessertItems[itemNumber - 1], quantity);
+                    totalBill += orders[orderIndex].getTotalPrice();
+                    orderIndex++;
+                } 
+                else 
+                {
+                    System.out.println("Invalid item number. Please try again.");
+                }
+            } 
+            else if (choice == 4) 
+            {
+                // Similar process for Drinks
+            	System.out.println("Drinks:");
+                FoodItem[] drinkItems = drinks.getDrinks();
+                int itemIndex = 1;
+                for (FoodItem item : drinkItems) 
+                {
+                    System.out.println(itemIndex + ": " + item.getName() + " - Rs. " + item.getPrice());
+                    itemIndex++;
+                }
+
+                System.out.print("Enter the item number: ");
+                int itemNumber = scanner.nextInt();
+                System.out.print("Enter the quantity: ");
+                int quantity = scanner.nextInt();
+
+                if (itemNumber >= 1 && itemNumber <= drinkItems.length) 
+                {
+                    orders[orderIndex] = new Order(drinkItems[itemNumber - 1], quantity);
+                    totalBill += orders[orderIndex].getTotalPrice();
+                    orderIndex++;
+                } 
+                else 
+                {
+                    System.out.println("Invalid item number. Please try again.");
+                }
+            } 
+            else 
+            {
+                System.out.println("Invalid choice. Please try again.");
+            }
+
+        // Display the bill
+        System.out.println("Your order summary:");
+        for (int i = 0; i < orderIndex; i++) 
+        {
+            Order order = orders[i];
+            System.out.println(
+                    order.getItem().getName() + " x " + order.getQuantity() + " - Rs. " + order.getTotalPrice());
+        }
+        System.out.println("----------------------------");
+        System.out.println("Total Bill: Rs. " + totalBill);
+        System.out.println("Thank you for using the Food Ordering System!");
+    }
+   }
 }
-
-
